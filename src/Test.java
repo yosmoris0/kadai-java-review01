36f1e14aed903a86dde573c54246be6782a9605d
yosmoris0 <morishige.yoshihito0531@outlook.jp>
2023-01-30 20:12:49 +0900

    Javaプログラミング入門学習終了

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    //mainメソッドです！
+    public static void main(String[] args) {
+        System.out.println("TechAcademy");
+        System.out.println("Java");
+        System.out.println("学習を始めましょう!");
+        System.out.println("こっちは表示されるよ！");
+        
+    }
+}
---
+++ src/UseVariable.java
@@ -0,0 +1,20 @@
+
+public class UseVariable {
+
+    public static void main(String[] args) {
+        int number1;
+        String str1;
+        number1 = 1;
+        str1 = "abc";
+        System.out.println(number1);
+        System.out.println(str1);
+        int number2 = 100;
+        String str2 = "XYZ";
+        System.out.println(number2);
+        System.out.println(str2);
+        String str = "何らかのデータ";
+        str =  "別の何か";
+        System.out.println(str);
+    }
+
+}
